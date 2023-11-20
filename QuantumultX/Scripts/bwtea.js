628ab978eab544194710eade350bf9b9af08d10b
treasunew <fullofyou1314@gmail.com>
2023-11-20 17:49:21 +0800

    update remote resources

--- QuantumultX/Scripts/bwtea.js
+++ QuantumultX/Scripts/bwtea.js
@@ -1,7 +1,7 @@
 /* 霸王茶姬
 [rewrite_local]
 # > 替换token，达到换账号的功能 自己手动修改token的值
-^https?:\/\/qmwebapi\.qmai\.cn\/web\/seller\/oauth\/flash-sale-login.* url script-request-body bwtea.js
+^https?:\/\/qmwebapi\.qmai\.cn\/web\/seller\/oauth\/flash-sale-login.* url script-request-body https://raw.githubusercontent.com/treasunew/SillyLib/main/QuantumultX/Scripts/bwtea.js
 [mitm]
 hostname = qmwebapi.qmai.cn
 */
