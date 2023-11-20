f0c865299a140464dedb34b5937e18989b8d7694
treasunew <fullofyou1314@gmail.com>
2023-11-20 19:10:45 +0800

    fix typo

--- QuantumultX/Scripts/bwtea.js
+++ QuantumultX/Scripts/bwtea.js
@@ -1,7 +1,7 @@
 /* 霸王茶姬
 [rewrite_local]
 # > 替换token，达到换账号的功能 自己手动修改token的值
-^https?:\/\/qmwebapi\.qmai\.cn\/web\/seller\/oauth\/flash-sale-login.* url script-request-body https://raw.githubusercontent.com/treasunew/SillyLib/main/QuantumultX/Scripts/bwtea.js
+^http[s]?:\/\/qmwebapi\.qmai\.cn\/web\/seller\/oauth\/flash-sale-login.*$ url script-request-body https://raw.githubusercontent.com/treasunew/SillyLib/main/QuantumultX/Scripts/bwtea.js
 [mitm]
 hostname = qmwebapi.qmai.cn
 */
@@ -10,7 +10,7 @@ hostname = qmwebapi.qmai.cn
 const tk = ''
 
 var body = $response.body
-var obj = JSON.parse(dody)
+var obj = JSON.parse(body)
 
 obj['data']['token'] = tk
 body = JSON.stringify(obj)
