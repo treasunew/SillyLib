272186c6bb99fb8d20d4b8859d190dba99d559d2
treasunew <fullofyou1314@gmail.com>
2024-02-25 15:19:25 +0800

    refactor && add more sections && fix some erros

--- QuantumultX/Scripts/mnmilk.js
+++ QuantumultX/Scripts/mnmilk.js
@@ -1,20 +1,39 @@
 /* 
-蒙牛兑奶
+蒙牛兑奶，兑换提前点亮
 *******************************
 [rewrite_local]
 # > 修改返回体，使按钮变亮
-^http[s]?:\/\/mp-isv\.youzanyun\.com\/point\/item\/getItemDetail url script-response-body https://mirror.945688.xyz/https://raw.githubusercontent.com/treasunew/SillyLib/main/QuantumultX/Scripts/mnmilk.js 
+^http[s]?:\/\/mp-isv\.youzanyun\.com\/point\/item\/getItemDetail url script-response-body https://mirror.945688.xyz/https://raw.githubusercontent.com/treasunew/SillyLib/main/QuantumultX/Scripts/mnmilk.js
+^http[s]?:\/\/m\.pailifan\.com\/xcx\/v2\/mall_v2_goods_detail url script-response-body https://mirror.945688.xyz/https://raw.githubusercontent.com/treasunew/SillyLib/main/QuantumultX/Scripts/mnmilk.js
+^http[s]?:\/\/m\.pailifan\.com\/xcx\/v2\/exchange_order_confirm url script-response-body https://mirror.945688.xyz/https://raw.githubusercontent.com/treasunew/SillyLib/main/QuantumultX/Scripts/mnmilk.js
 
 [mitm]
-hostname = mp-isv.youzanyun.com
+hostname = mp-isv.youzanyun.com, m.pailifan.com
 */
+url = $request.url
+u1 = "/point/item/getItemDetail"
+u2 = "/xcx/v2/mall_v2_goods_detail"
+u3 = "/xcx/v2/exchange_order_confirm"
+
 var body = $response.body;
 var obj = JSON.parse(body);
 
-obj['data']['isExchange'] = true;
+if (url.indexOf(u1) != -1){
 
-body = JSON.stringify(obj);
-//console.log(body)
+	obj['data']['isExchange'] = true;
+	body = JSON.stringify(obj);
 
-$notify('蒙牛营养家', '商品点亮工具', '点亮成功')
+}else if (url.indexOf(u2) != -1){
+
+	obj['data']['is_not_time_exchange'] = 0;
+	obj['data']['spu']['exchange_limit']['exchange_type_value'] = 1;
+	//obj['data']['spu']['exchange_channel'] = 0
+	body = JSON.stringify(obj);
+	
+}else{
+	//obj['flag'] = 0;
+	//body = JSON.stringify(obj);
+	$done();
+}
+$notify("点亮人生", "蒙牛营养家", "点亮成功")
 $done(body);
--- Surge/Scripts/mnmilk.js
+++ Surge/Scripts/mnmilk.js
@@ -1,9 +1,27 @@
-let body = $response.body;
-let obj = JSON.parse(body);
+url = $request.url
+u1 = "/point/item/getItemDetail"
+u2 = "/xcx/v2/mall_v2_goods_detail"
+u3 = "/xcx/v2/exchange_order_confirm"
 
-obj['data']['isExchange'] = true;
+var body = $response.body;
+var obj = JSON.parse(body);
 
-mn = JSON.stringify(obj);
-///console.log(mn);
-$msg('蒙牛生活家', '', '点亮成功')
-$done({body: mn});
+if (url.indexOf(u1) != -1){
+
+	obj['data']['isExchange'] = true;
+	body = JSON.stringify(obj);
+
+}else if (url.indexOf(u2) != -1){
+
+	obj['data']['is_not_time_exchange'] = 0;
+	obj['data']['spu']['exchange_limit']['exchange_type_value'] = 1;
+	//obj['data']['spu']['exchange_channel'] = 0
+	body = JSON.stringify(obj);
+	
+}else{
+	//obj['flag'] = 0;
+	//body = JSON.stringify(obj);
+	$done();
+}
+$notification.post("点亮人生", "蒙牛营养家", "点亮成功")
+$done({body: body});
