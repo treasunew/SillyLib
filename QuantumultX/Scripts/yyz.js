f4a4e8d25c57c47c090bb2f5cd5498016428c6a5
treasunew <fullofyou1314@gmail.com>
2024-02-27 20:29:16 +0800

    refactor

--- QuantumultX/Scripts/yyz.js
+++ QuantumultX/Scripts/yyz.js
@@ -1,20 +1,48 @@
-/* 蒙牛营养值活动
+/* 蒙牛 伊利
 *******************************
 [rewrite_local]
 # > 抓openid和id
-^http[s]?:\/\/zaocan\.javamall\.cn\/zaocan-api\/member\/userMember url script-response-body https://mirror.945688.xyz/https://raw.githubusercontent.com/treasunew/SillyLib/main/QuantumultX/Scripts/yyz.js
+# > 抓openid和accesstoken
+^http[s]?:\/\/zaocan\.javamall\.cn\/zaocan-api\/member\/userMember url script-response-body yyz.js
+^http[s]?:\/\/wx-camp-180-02-applet-api\.mscampapi\.digitalyili\.com\/home\/page\/get\/game\/open\/time url script-request-header yyz.js
+^http[s]?:\/\/wx-camp-180-02-applet-api\.mscampapi\.digitalyili.com\/home\/page\/get\/task\/red\/hint url script-request-header yyz.js
 
 [mitm]
-hostname = zaocan.javamall.cn
+hostname = zaocan.javamall.cn, wx-camp-180-02-applet-api.mscampapi.digitalyili.com
 */
 
-var response = $response.body;
-res = JSON.parse(response);
-id = res.data.id;
-openid = res.data.openid;
+var url = $request.url;
 
-var title = "蒙牛营养家";
-var subtitle = "openid 和 id 获取成功";
-var msg = 'id: '+ id + '\nopenid: ' +openid;
-$notify(title, subtitle, msg);
-$done()
+
+u1 = '/zaocan.javamall.cn/zaocan-api/member/userMember'
+u2 = '/wx-camp-180-02-applet-api.mscampapi.digitalyili.com/home/page/get/task/red/hint'
+
+if (url.indexOf(u1) != -1){
+	var response = $response.body;
+	res = JSON.parse(response);
+	id = res.data.id;
+	openid = res.data.openid;
+
+	var title = "蒙牛营养家";
+	var subtitle = "openId 和 id 获取成功";
+	var msg = 'id: '+ id + '\nopenid: ' +openid;
+	$notify(title, subtitle, msg);
+	
+}else if(url.indexOf(u2) != -1){
+	var headers = $request.headers;
+	// console.log(`Headers: ${headers}`)
+	var openId = url.match(/openId=([^&]*)/)[1];
+	// console.log(`openId: ${openId}`);
+	var accessToken = headers.access_token;
+	// console.log(`accessToken: ${accessToken}`)
+	var title = "伊利";
+	var subtitle = "openId 和 accessToken 获取成功";
+	var msg = `openId: ${openId}\naccessToken: ${accessToken}`;
+	$notify(title, subtitle, msg);
+	
+}else{
+	$done();
+	
+}
+
+$done();
